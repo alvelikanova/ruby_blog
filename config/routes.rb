bb10168764f241613cdb4eff375fb77ceea3516b
Alena <velikanova.alenka@gmail.com>
2015-03-28 14:40:38 +0300

    Users list page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+
+  get 'users/show'
+
+  get 'users/new'
+
+  resources :users
+
   get 'static_pages/home'
 
   get 'static_pages/help'
 
-  get 'users/new'
-
+  match '/signup', to: 'users#new', via: 'get'
+ 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
