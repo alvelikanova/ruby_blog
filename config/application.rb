4a71d2c76848a3378d13f2aafa93fe65257ab05a
Alena <velikanova.alenka@gmail.com>
2015-03-28 15:02:02 +0300

    Custom styles

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,6 @@ module RubyBlog
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.precompile += %w(*.png *.gif *.jpg *.jpeg) 
   end
 end
